5035a67e5fa1feb885ac4c80cd12be211cbeae9f
Shashank <s.shak101@gmail.com>
2020-09-17 18:22:50 -0400

    added bird

--- screens/optionscreen.js
+++ screens/optionscreen.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Image  } from 'react-native';
+
+const image = require('../assets/tie.png')
 
 export default function Optionscreen() {
     return (
       <SafeAreaView style={styles.container}>
-
+        <View style={styles.container}/>
+        <View style={styles.container}>
+          <Image source={image} style={styles.lul}/>
+        </View>
+        <View style={styles.container}/>
       </SafeAreaView>
     );
   }
   const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'white',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    lul: {
+      resizeMode: 'center',
     },
   });
