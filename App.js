871790dac0807ccbf8bae64597ccba7485616a66
Shashank <s.shak101@gmail.com>
2020-09-17 18:27:49 -0400

    cleaning up

--- App.js
+++ App.js
@@ -15,25 +15,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignContent: 'center',
     justifyContent: 'center'
-  },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center"
-  },
-  button: {
-    width: '90%',
-    height: 50,
-    borderRadius: 5,
-    backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center'
-  },
-  linktext: {
-    fontWeight: 'bold',
-    fontFamily: 'Roboto',
-    fontSize: 17,
-    letterSpacing: 2
   }
 });
